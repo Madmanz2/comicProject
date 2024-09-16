983f33a7ea046f63f159dafb435d64157bf1d2ff
212894 <212894@HS-120-03.yssd.org>
2024-09-16 08:10:53 -0400

    update

--- comicProject/src/comicProject/Panel1.java
+++ comicProject/src/comicProject/Panel1.java
@@ -23,7 +23,17 @@ public class Panel1 extends JPanel {
 		Font f = new Font("Helvetica", Font.PLAIN, 35);
 		g.setFont(f);
 		g.drawString("Arives at Emergency Room", 75, 40);
-		g.drawLine(325, 150, 325, 350);
+		g.setColor(Color.blue);
+		g.drawLine(325, 200, 325, 350);
+		g.setColor(new Color(238,207,180));
+		g.drawLine(325, 225, 350, 250);
+		g.drawLine(325, 225, 275, 200);
+		g.setColor(new Color(0,0,0));
+		g.drawLine(200, 500, 200, 50);
+		g.drawLine(500, 500, 500, 50);
+		g.drawLine(200, 50, 500, 50);
+		g.fillOval(350, 125, 10, 10);
+		g.fillOval(325, 125, 10, 10);
 	}
 	
 	
